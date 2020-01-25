381d829c460d0735f3f45201399350ca033c31f0
andrey <andreykolymagin@yandex.ru>
2020-01-25 11:59:37 +0300

    airplane adapter getValue "while"-version

--- src/main/java/com/mcb/creditfactory/service/airplane/AirplaneAdapter.java
+++ src/main/java/com/mcb/creditfactory/service/airplane/AirplaneAdapter.java
@@ -5,22 +5,40 @@ import com.mcb.creditfactory.external.CollateralObject;
 import com.mcb.creditfactory.external.CollateralType;
 import com.mcb.creditfactory.model.AirplaneCostEvaluation;
 import com.mcb.creditfactory.model.CarCostEvaluation;
+import com.mcb.creditfactory.repository.AirplaneCostEvaluationRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Date;
 import java.util.Iterator;
-
+@Service
 @AllArgsConstructor
 public class AirplaneAdapter implements CollateralObject {
       private AirplaneDto airplaneDto;
 
+      @Autowired
+      private AirplaneCostEvaluationRepository airplaneCostEvaluationRepository;
+
+
 
     @Override
     public BigDecimal getValue() {
+
         Long id = airplaneDto.getId();
-
-
+        Iterable<AirplaneCostEvaluation> allByAirplaneId = airplaneCostEvaluationRepository.findAllByAirplaneId(id);
+        Iterator<AirplaneCostEvaluation> iterator = allByAirplaneId.iterator();
+        Date lastDate = new Date(0);
+        while (iterator.hasNext()){
+            AirplaneCostEvaluation next = iterator.next();
+            Date date = next.getDate();
+            if(date.after(lastDate)){
+                lastDate=date;
+            }
+        }
+//        allByAirplaneId.spliterator();
         return null;
     }
 
