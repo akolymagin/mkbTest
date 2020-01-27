b5344b88395856b58f7b510b985e19586811f05c
andrey <andreykolymagin@yandex.ru>
2020-01-27 15:19:19 +0300

    Added postman JsonCollection to resources

--- src/main/java/com/mcb/creditfactory/dto/collateralCostEvaluation/CostEvaluation.java
+++ src/main/java/com/mcb/creditfactory/dto/collateralCostEvaluation/CostEvaluation.java
@@ -1,6 +1,14 @@
 package com.mcb.creditfactory.dto.collateralCostEvaluation;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.mcb.creditfactory.dto.collateral.AirplaneDto;
+import com.mcb.creditfactory.dto.collateral.CarDto;
 
-
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = CarCostEvaluationDto.class),
+        @JsonSubTypes.Type(value = AirplaneCostEvaluationDto.class)
+})
 public interface CostEvaluation {
 }
